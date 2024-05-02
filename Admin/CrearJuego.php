6a1803ed0ab9b484059b4294bd8f4c803ef78ba7
kevin <KevinS9sotob@hotmail.com>
2024-05-02 20:14:13 +0100

    imagenes y seguridad

--- Admin/CrearJuego.php
+++ Admin/CrearJuego.php
@@ -1,121 +1,107 @@
 <?php
-
 session_start();
 
 $html = "";
 
-if (!isset($_SESSION['Tipo']) || $_SESSION['Tipo'] !== "administrador") {
+if (!isset($_SESSION['tipo_usuario']) || $_SESSION['tipo_usuario'] !== "administrador") {
     $html .= "<div class='NoAdmin'>";
-    $html .= "No has iniciado sesión";
-    $html .= "<button onclick='window.location.href=\"index.php\"'>Hazlo Aquí</button>";
+    $html .= "No has iniciado sesión como administrador";
+    $html .= "<button onclick='window.location.href=\"../index.php\"'>Hazlo Aquí</button>";
     $html .= "</div>";
    
     echo $html;
-}
+} else {
+    session_abort();
 
-else{
-    
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $nombre = $_POST["nombre"];
+        // Manejo de la imagen subida
+        $imagen = $_FILES["imagen"]["name"];
+        $imagen_temp = $_FILES["imagen"]["tmp_name"];
+        $descripcion = $_POST["descripcion"];
+        $fecha = $_POST["fecha"];
+        $precio = floatval($_POST["precio"]);
+        $clave = $_POST["clave"];
+        
+        // Carpeta de destino para las imágenes subidas
+        $carpeta_destino = "../ImagenesJuegos/";
 
-session_abort();
+        try {
+            require '../bd.php';
 
-if($_SERVER["REQUEST_METHOD"]=="POST"){
-    $nombre = $_POST["nombre"];
-    $imagen = $_POST["imagen"];
-    $descripcion = $_POST["descripcion"];
-    $fecha = $_POST["fecha"];
-    $precio = floatval($_POST["precio"]);
-    $clave = $_POST["clave"];
-    
-    
-    try {
-        require '../bd.php';
-        $ins = "INSERT INTO `juegos` (`JuegoID`, `nombre`, `imagen`, `descripcion`, `fecha_salida`, `precio`, `clave`) VALUES (NULL, '$nombre', '$imagen', '$descripcion', '$fecha', '$precio', '$clave')";
-        $resul = $bd->query($ins);
-        if ($resul) {
-            header("Location:../Admin.php");
+            // Mueve la imagen de la carpeta temporal a la carpeta de destino
+            if (move_uploaded_file($imagen_temp, $carpeta_destino . $imagen)) {
+                // Inserta los datos en la base de datos
+                $ins = "INSERT INTO `juegos` (`JuegoID`, `nombre`, `imagen`, `descripcion`, `fecha_salida`, `precio`, `clave`) VALUES (NULL, '$nombre', '$imagen', '$descripcion', '$fecha', '$precio', '$clave')";
+                $resul = $bd->query($ins);
+                if ($resul) {
+                    header("Location:../Admin.php");
+                }
+            } else {
+                echo "Error al subir la imagen.";
+            }
+        } catch (PDOException $e) {
+            echo "Error: Comprueba que los datos introducidos no están ya en la base de datos";
         }
-    } catch (PDOException $e) {
-        echo "Error: Comprueba que los datos introducidos no estan ya en la base de datos";
     }
 }
-
 ?>
+
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Gaming World</title>
-        <link rel="stylesheet" href="../css/styles.css">
+<head>
+    <meta charset="UTF-8">
+    <title>Gaming World</title>
+    <link rel="stylesheet" href="../css/styles.css">
+    <?php require '../menu2.php'; ?>
+</head>
+<body>
+    <!-- Formulario de creación de Juegos -->
+    <form class="formularioCrear" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST" enctype="multipart/form-data">
+        <h1 class="formularioCrear-tituloPrincipal">Introduzca los datos del Juego</h1>
         
-        <?php require '../menu2.php'; ?>
-        
-    </head>
-    <body>
-        
-        <!-- Formulario de creacion de Juegos -->
-        <form class="formularioCrear" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
-            
-            <h1 class="formularioCrear-tituloPrincipal">Introduzca los datos del Juego</h1>
-            
-            <!-- Contenedor de los datos -->
-            <div class="formularioCrear-container">
-                
-                <!-- Nombre -->
-                <div class="formularioCrear-container-datos">
-                    
-                    <p class="formularioCrear-container-datos-nombre">Nombre</p>
-                    <input type="text" name="nombre">
-                    
-                </div>
-                
-                <!-- Imagen -->
-                <div class="formularioCrear-container-datos">
-                    
-                    <p class="formularioCrear-container-datos-imagen">Imagen</p>
-                    <input type="text" name="imagen">
-                    
-                </div>
-                
-                <!-- Descripcion -->
-                <div class="formularioCrear-container-datos">
-                    
-                    <p class="formularioCrear-container-datos-descripcion">Descripcion</p>
-                    <input type="textarea" name="descripcion">
-                    
-                </div>
-                
-                <!-- Fecha  -->
-                <div class="formularioCrear-container-datos">
-                    
-                    <p class="formularioCrear-container-datos-fecha">Fecha</p>
-                    <input type="date" name="fecha">
-                    
-                </div>
-                
-                <!-- Precio -->
-                <div>
-                    
-                    <p class="formularioCrear-container-datos-precio">Precio</p>
-                    <input type="text" name="precio">
-                    
-                </div>
-                
-                <!-- Clave -->
-                <div class="formularioCrear-container-datos">
-                    
-                    <p class="formularioCrear-container-datos-clave">Clave</p>
-                    <input type="text" name="clave">
-                    
-                </div>
-                
-                <br></br>
-                
-                <input type="submit">
-                
+        <!-- Contenedor de los datos -->
+        <div class="formularioCrear-container">
+            <!-- Nombre -->
+            <div class="formularioCrear-container-datos">
+                <p class="formularioCrear-container-datos-nombre">Nombre</p>
+                <input type="text" name="nombre">
             </div>
-    </body>
+            
+            <!-- Imagen -->
+            <div class="formularioCrear-container-datos">
+                <p class="formularioCrear-container-datos-imagen">Imagen</p>
+                <input type="file" name="imagen">
+            </div>
+            
+            <!-- Descripcion -->
+            <div class="formularioCrear-container-datos">
+                <p class="formularioCrear-container-datos-descripcion">Descripcion</p>
+                <input type="textarea" name="descripcion">
+            </div>
+            
+            <!-- Fecha  -->
+            <div class="formularioCrear-container-datos">
+                <p class="formularioCrear-container-datos-fecha">Fecha</p>
+                <input type="date" name="fecha">
+            </div>
+            
+            <!-- Precio -->
+            <div>
+                <p class="formularioCrear-container-datos-precio">Precio</p>
+                <input type="text" name="precio">
+            </div>
+            
+            <!-- Clave -->
+            <div class="formularioCrear-container-datos">
+                <p class="formularioCrear-container-datos-clave">Clave</p>
+                <input type="text" name="clave">
+            </div>
+            
+            <br></br>
+            
+            <input type="submit">
+        </div>
+    </form>
+</body>
 </html>
-
-<?php
-}
-?>
--- PagPrincipal.php
+++ PagPrincipal.php
@@ -1,114 +1,97 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Gaming World</title>
-    </head>
-    <body>
-        <div>
-            <?php
-            require 'menu.php';
-            require 'bd.php';
-            
-            
+<?php
+require 'menu.php';
+require 'bd.php';
 
-            echo "<link rel='stylesheet' href='css/styles.css'>";
+echo "<link rel='stylesheet' href='css/styles.css'>";
 
-            // Num de juegos por pagina
-            $registros_por_pagina = 16;
+// Num de juegos por pagina
+$registros_por_pagina = 16;
 
-            // Obtener la paggina actual
-            $pagina_actual = isset($_GET['pagina']) ? $_GET['pagina'] : 1;
+// Obtener la paggina actual
+$pagina_actual = isset($_GET['pagina']) ? $_GET['pagina'] : 1;
 
-            // Offset por donde empezar a coger datos
-            $offset = ($pagina_actual - 1) * $registros_por_pagina;
+// Offset por donde empezar a coger datos
+$offset = ($pagina_actual - 1) * $registros_por_pagina;
 
-            // Numero total de juegos
-            $total_registros_query = "SELECT COUNT(*) AS total FROM juegos";
-            $total_registros_result = $bd->query($total_registros_query);
-            $total_registros = $total_registros_result->fetch()['total'];
+// Numero total de juegos
+$total_registros_query = "SELECT COUNT(*) AS total FROM juegos";
+$total_registros_result = $bd->query($total_registros_query);
+$total_registros = $total_registros_result->fetch()['total'];
 
-            $total_paginas = ceil($total_registros / $registros_por_pagina);
+$total_paginas = ceil($total_registros / $registros_por_pagina);
 
-            // Consulta SQL (para que coja 16 por cada pagina)
-            $sel = "SELECT
-                    juegos.juegoID,
-                    juegos.nombre,
-                    juegos.imagen,
-                    juegos.descripcion,
-                    juegos.fecha_salida,
-                    juegos.precio,
-                    GROUP_CONCAT(generos.nombre) AS generos
-                FROM juegos
-                INNER JOIN juegos_generos ON juegos_generos.juegoID = juegos.juegoID
-                INNER JOIN generos ON generos.generoID = juegos_generos.generoID
-                GROUP BY juegos.juegoID
-                ORDER BY juegos.fecha_salida DESC
-                LIMIT $registros_por_pagina OFFSET $offset";
+// Consulta SQL (para que coja 16 por cada pagina)
+$sel = "SELECT
+            juegos.juegoID,
+            juegos.nombre,
+            juegos.imagen,
+            juegos.descripcion,
+            juegos.fecha_salida,
+            juegos.precio,
+            GROUP_CONCAT(generos.nombre) AS generos
+        FROM juegos
+        INNER JOIN juegos_generos ON juegos_generos.juegoID = juegos.juegoID
+        INNER JOIN generos ON generos.generoID = juegos_generos.generoID
+        GROUP BY juegos.juegoID
+        ORDER BY juegos.fecha_salida DESC
+        LIMIT $registros_por_pagina OFFSET $offset";
 
-            $juegos = $bd->query($sel);
+$juegos = $bd->query($sel);
 
-            // Lista de juegos
-            
-            $html = "";
-            
-            $html .= "<div class='titulo'>";
-            $html .= "<h1>GamingWorld</h1>";
-            $html .= "</div>";
-            
-            $html .= "<div class='calendario-link'>";
-            $html .= "<button onclick=\"location.href = 'historico.php'\">Ver Historial</button>";
-            $html .= "</div>";
+// Lista de juegos
+$html = "";
+$html .= "<div class='titulo'>";
+$html .= "<h1>GamingWorld</h1>";
+$html .= "</div>";
+$html .= "<div class='calendario-link'>";
+$html .= "<button onclick=\"location.href = 'historico.php'\">Ver Historial</button>";
+$html .= "</div>";
+$html .= "<div class='contenedor-juegos'>";
 
-            $html .= "<div class='contenedor-juegos'>";
+foreach ($juegos as $juego) {
+    $html .= "<div class='contenedor-juegos-content'>";
+    $html .= "<a href='PagJuego.php?juegoID={$juego['juegoID']}' class='enlace-juego'>";
+    $html .= "<div class='juego'>";
+    $html .= "<div class='contenido'>";
 
-            foreach ($juegos as $juego) {
-                $html .= "<div class='contenedor-juegos-content'>";
-                $html .= "<a href='PagJuego.php?juegoID={$juego['juegoID']}' class='enlace-juego'>";
-                $html .= "<div class='juego'>";
-                $html .= "<div class='contenido'>";
+    // Nombre
+    $html .= "<h2>{$juego['nombre']}</h2>";
 
-                // Nombre
-                $html .= "<h2>{$juego['nombre']}</h2>";
+    //Imagen
+    $html .= "<img src='ImagenesJuegos/{$juego['imagen']}' alt='{$juego['nombre']}'>";
 
-                // Imagen
-                $html .= "<img src='{$juego['imagen']}' alt='{$juego['nombre']}'>";
+    // Precio
+    $html .= "<p>Precio: {$juego['precio']}</p>";
 
-                // Precio
-                $html .= "<p>Precio: {$juego['precio']}</p>";
+    // Generos 
+    $generos = explode(",", $juego["generos"]);
+    $html .= "<p>Géneros: ";
+    foreach ($generos as $genero) {
+        $query = "SELECT generoID FROM generos WHERE nombre = ?";
+        $statement = $bd->prepare($query);
+        $statement->execute([$genero]);
+        $resultado = $statement->fetch();
+        $generoID = isset($resultado['generoID']) ? urlencode($resultado['generoID']) : '';
+        $html .= "<a class='generos' href='PagGenero.php?generoID=$generoID' id='$generoID'>$genero</a>, ";
+    }
+    $html .= "</p>";
+    $html .= "</div>";
+    $html .= "</div>";
+    $html .= "</a>";
+    $html .= "</div>";
+}
 
-                // Generos 
-                $generos = explode(",", $juego["generos"]);
-                $html .= "<p>Géneros: ";
-                foreach ($generos as $genero) {
-                    $query = "SELECT generoID FROM generos WHERE nombre = ?";
-                    $statement = $bd->prepare($query);
-                    $statement->execute([$genero]);
-                    $resultado = $statement->fetch();
-                    $generoID = isset($resultado['generoID']) ? urlencode($resultado['generoID']) : '';
-                    $html .= "<a class='generos' href='PagGenero.php?generoID=$generoID' id='$generoID'>$genero</a>, ";
-                }
-                $html .= "</p>";
-                $html .= "</div>";
-                $html .= "</div>";
-                $html .= "</a>";
-                $html .= "</div>";
-            }
+$html .= "</div>";
 
-            $html .= "</div>";
+// Paginación botones
+$html .= "<div class='paginacion'>";
+for ($i = 1; $i <= $total_paginas; $i++) {
+    $html .= "<form action='' method='GET' style='display:inline;'>";
+    $html .= "<input type='hidden' name='pagina' value='$i'>";
+    $html .= "<button type='submit'>$i</button>";
+    $html .= "</form>";
+}
+$html .= "</div>";
 
-            // Paginación botonoes
-            $html .= "<div class='paginacion'>";
-            for ($i = 1; $i <= $total_paginas; $i++) {
-                $html .= "<form action='' method='GET' style='display:inline;'>";
-                $html .= "<input type='hidden' name='pagina' value='$i'>";
-                $html .= "<button type='submit'>$i</button>";
-                $html .= "</form>";
-            }
-            $html .= "</div>";
-
-            echo $html;
-            ?>
-        </div>
-    </body>
-</html>
+echo $html;
+?>
