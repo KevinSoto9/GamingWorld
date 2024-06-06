f79dc3ab15703105458d2e4b709a07367de33e8e
kevin <KevinS9sotob@hotmail.com>
2024-06-06 09:09:26 +0100

    prueba4

--- bd.php
+++ bd.php
@@ -1,2 +1,2 @@
 <?php
-$bd = new PDO("mysql:host=ec2-3-208-26-92.compute-1.amazonaws.com;dbname=GamingWorld", "ubuntu", "1234");
+$bd = new PDO("mysql:host=ec2-3-208-26-92.compute-1.amazonaws.com;dbname=GamingWorld", "prueba", "1234");
