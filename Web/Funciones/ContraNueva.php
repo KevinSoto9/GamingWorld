9081af6592af109a25829817912b03ebc9433902
kevin <KevinS9sotob@hotmail.com>
2024-06-14 20:10:16 +0100

    x

--- Web/Funciones/ContraNueva.php
+++ Web/Funciones/ContraNueva.php
@@ -74,7 +74,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <title>Recuperar Contraseña</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/cssPlus/cssPlus.css">
+    <link rel="stylesheet" href="../../assets/cssPlus/cssPlus.css">
 </head>
 <body>
 <div class="container mt-5">
