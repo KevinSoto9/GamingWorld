412440dcf860defec86a58e6c91bfa3d2644d67e
kevin <KevinS9sotob@hotmail.com>
2024-06-15 12:15:45 +0100

    cambios paginas individuales

--- Web/PaginasIndividuales/PagDesarrollador.php
+++ Web/PaginasIndividuales/PagDesarrollador.php
@@ -84,7 +84,7 @@ if (!isset($_SESSION['UsuarioID']) || $_SESSION['UsuarioID'] === null) {
             foreach ($juegos as $juego) {
                 ?>
                 <div class="col-md-3">
-                    <div class="card mb-5 bg-dark text-white">
+                    <div class="card mb-2 bg-dark h-100 text-white">
                         <a href='PagJuego.php?juegoID=<?= $juego['juegoID'] ?>' class='enlace-juego text-white'>
                             <img class='card-img-top' src='../../Imagenes/ImagenesJuegos/<?= $juego['nombre'].".jpg" ?>' alt='<?= $juego['nombre'] ?>'>
                             <div class='card-body'>
